3bfc9566ed406c2780485a0e7b41abbccd741ee4
omghonasgi <157753451+omghonasgi@users.noreply.github.com>
2025-04-16 16:45:52 -0500

    Add files via upload

--- comm.c
+++ comm.c
@@ -19,24 +19,31 @@ int read_from_pi(int devid)
 
 }
 
-void steering(int gpio, int pos)
+void steering(int gpio, int angle)
 {
     // Task-3: 
     // Your code goes here (Use Lab 05 for reference)
     // Check the project document to understand the task
-}
+            int pwm_signal = ((2400 - 544 *(angle/180))) + 544;
+            gpio_write(gpio, ON);
+            delay_usec(pwm_signal);
+            gpio_write(gpio, OFF);
+            delay_usec(20000 - pwm_signal);
+     
+    }
+
 
 
 int main()
 {
     // initialize UART channels
-    ser_setup(0); // uart0
-    ser_setup(1); // uart1
-    int pi_to_hifive = 1; //The connection with Pi uses uart 1
-    int lidar_to_hifive = 0; //the lidar uses uart 0
+    //ser_setup(0); // uart0
+    //ser_setup(1); // uart1
+    //int pi_to_hifive = 1; //The connection with Pi uses uart 1
+    //int lidar_to_hifive = 0; //the lidar uses uart 0
     
-    printf("\nUsing UART %d for Pi -> HiFive", pi_to_hifive);
-    printf("\nUsing UART %d for Lidar -> HiFive", lidar_to_hifive);
+    //printf("\nUsing UART %d for Pi -> HiFive", pi_to_hifive);
+    //printf("\nUsing UART %d for Lidar -> HiFive", lidar_to_hifive);
     
     //Initializing PINs
     gpio_mode(PIN_19, OUTPUT);
@@ -49,9 +56,9 @@ int main()
 
     while (1) {
 
-        auto_brake(lidar_to_hifive); // measuring distance using lidar and braking
-        int angle = read_from_pi(pi_to_hifive); //getting turn direction from pi
-        printf("\nangle=%d", angle) 
+        //auto_brake(lidar_to_hifive); // measuring distance using lidar and braking
+        //int angle = read_from_pi(pi_to_hifive); //getting turn direction from pi
+        //printf("\nangle=%d", angle) 
         int gpio = PIN_19; 
         for (int i = 0; i < 10; i++){
             // Here, we set the angle to 180 if the prediction from the DNN is a positive angle
@@ -60,16 +67,21 @@ int main()
             // You are welcome to pass the angle values directly to the steering function.
             // If the servo function is written correctly, it should still work,
             // only the movements of the servo will be more subtle
+
+            
+            
+
+            /*
             if(angle>0){
                 steering(gpio, 180);
             }
             else {
                 steering(gpio,0);
             }
-            
+            */
             // Uncomment the line below to see the actual angles on the servo.
             // Remember to comment out the if-else statement above!
-            // steering(gpio, angle);
+            steering(gpio, 30);
         }
 
     }
